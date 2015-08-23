5b14df254b99f1d72259d88c575d6f3ce525b29f
Nathan Racklyeft <loudnate@gmail.com>
2015-08-22 23:05:58 -0700

    Displaying the app version in the menu

---
+++ RileyLink/MenuHeaderTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuHeaderTableViewCell.h
+//  RileyLink
+//
+//  Created by Nathan Racklyeft on 8/22/15.
+//  Copyright © 2015 Pete Schwamb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuHeaderTableViewCell : UITableViewCell
+
+@property (nonatomic, nullable, copy) NSString *title;
+
+@end

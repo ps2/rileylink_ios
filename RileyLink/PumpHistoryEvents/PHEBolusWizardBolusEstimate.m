0591e8096a9fc484aaa9b5f5d06a24e4d3987c11
Pete Schwamb <pete@schwamb.net>
2015-11-20 19:41:11 -0600

    Add new history event types

---
+++ RileyLink/PumpHistoryEvents/PHEBolusWizardBolusEstimate.m
@@ -0,0 +1,18 @@
+//
+//  PHEBolusWizardBolusEstimate.m
+//
+
+#import "PHEBolusWizardBolusEstimate.h"
+
+@implementation PHEBolusWizardBolusEstimate
+
++ (int) eventTypeCode {
+  return 0x5b;
+}
+
+
+- (int) length {
+  return 20;
+}
+
+@end

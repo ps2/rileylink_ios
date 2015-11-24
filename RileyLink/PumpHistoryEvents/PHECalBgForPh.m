eea5039e3cd962d4f5d48915c4c6c735a8b3a8bb
Pete Schwamb <pete@schwamb.net>
2015-11-23 22:45:33 -0600

    CalBGForPH

--- RileyLink/PumpHistoryEvents/PHECalBgForPh.m
+++ RileyLink/PumpHistoryEvents/PHECalBgForPh.m
@@ -10,9 +10,20 @@
   return 0x0a;
 }
 
+- (int) amount {
+  return (([self byteAt:6] & 0b10000000) << 1) + [self byteAt:1];
+}
 
 - (int) length {
   return 7;
+}
+
+- (NSDictionary*) asJSON {
+  NSMutableDictionary *base = [[super asJSON] mutableCopy];
+  if ([self amount] > 0) {
+    base[@"amount"] = [NSNumber numberWithInt:[self amount]];
+  }
+  return base;
 }
 
 @end

815fba44fe1e43af6b92135dbee6b0967dfba4a3
Pete Schwamb <pete@schwamb.net>
2016-02-28 00:31:06 -0600

    Suggestions per @loudnate

--- MinimedKit/RFTools.swift
+++ MinimedKit/RFTools.swift
@@ -11,24 +11,21 @@ import Foundation
 func decode4b6b(rawData: NSData) -> NSData? {
   let codes:Dictionary<UInt, UInt8> = [21: 0, 49: 1, 50: 2, 35: 3, 52: 4, 37: 5, 38: 6, 22: 7, 26: 8, 25: 9, 42: 10, 11: 11, 44: 12, 13: 13, 14: 14, 28: 15]
   var buffer = [UInt8]()
-  let bytes = UnsafeBufferPointer<UInt8>(start:UnsafePointer<UInt8>(rawData.bytes), count:rawData.length)
+  let bytes: [UInt8] = rawData[0..<rawData.length]
   var availBits: UInt = 0
   var x: UInt = 0
-  var hiNibble: UInt8?
-  var loNibble: UInt8?
-  for var i = 0; i < rawData.length; i++ {
-    x = (x << 8) + UInt(bytes[i])
+  for byte in bytes {
+    x = (x << 8) + UInt(byte)
     availBits += 8
     if availBits >= 12 {
-      hiNibble = codes[x >> (availBits - 6)]
-      loNibble = codes[(x >> (availBits - 12)) & 0b111111]
-      
-      if hiNibble != nil && loNibble != nil {
-        let decoded: UInt8 = UInt8((hiNibble! << 4) + loNibble!)
-        buffer.append(decoded)
-      } else {
+      guard let
+        hiNibble = codes[x >> (availBits - 6)],
+        loNibble = codes[(x >> (availBits - 12)) & 0b111111]
+        else {
         return nil
       }
+      let decoded: UInt8 = UInt8((hiNibble << 4) + loNibble)
+      buffer.append(decoded)
       availBits -= 12;
       x = x & (0xffff >> (16-availBits));
     }
@@ -39,16 +36,16 @@ func decode4b6b(rawData: NSData) -> NSData? {
 func encode4b6b(rawData: NSData) -> NSData {
   var buffer = [UInt8]()
   let codes: [UInt] = [21,49,50,35,52,37,38,22,26,25,42,11,44,13,14,28]
-  let bytes = UnsafeBufferPointer<UInt8>(start:UnsafePointer<UInt8>(rawData.bytes), count:rawData.length)
+  let bytes: [UInt8] = rawData[0..<rawData.length]
   var acc: UInt = 0x0
   var bitcount: UInt = 0
-  for var i=0; i < rawData.length; i++ {
+  for byte in bytes {
     acc <<= 6;
-    acc |= codes[Int(bytes[i] >> 4)]
+    acc |= codes[Int(byte >> 4)]
     bitcount += 6;
     
     acc <<= 6;
-    acc |= codes[Int(bytes[i] & 0x0f)];
+    acc |= codes[Int(byte & 0x0f)];
     bitcount += 6;
     
     while bitcount >= 8 {
@@ -57,7 +54,7 @@ func encode4b6b(rawData: NSData) -> NSData {
       acc &= (0xffff >> (16-bitcount));
     }
   }
-  if (bitcount > 0) {
+  if bitcount > 0 {
     acc <<= (8-bitcount);
     buffer.append(UInt8(acc) & 0xff);
   }

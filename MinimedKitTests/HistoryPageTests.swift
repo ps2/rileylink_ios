746388fa1d7b206435809c264e46a16b9a5dd25f
Nathan Racklyeft <loudnate@gmail.com>
2016-04-09 22:36:39 -0700

    Compliler warning

--- MinimedKitTests/HistoryPageTests.swift
+++ MinimedKitTests/HistoryPageTests.swift
@@ -122,7 +122,7 @@ class HistoryPageTests: XCTestCase {
 
       sara6e.timestamp.timeZone = NSTimeZone(forSecondsFromGMT: -5 * 60 * 60)
 
-      XCTAssertEqual(sara6e.dictionaryRepresentation["timestamp"] as! String, "2016-02-22T05:00:00Z")
+      XCTAssertEqual(sara6e.dictionaryRepresentation["timestamp"] as? String, "2016-02-22T05:00:00Z")
       
     } catch HistoryPage.Error.InvalidCRC {
       XCTFail("page decoding threw invalid crc")

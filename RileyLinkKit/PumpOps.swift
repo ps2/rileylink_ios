cb282b789ed91142bf6c511c34e86ed4ab1710b8
Nathan Racklyeft <loudnate@gmail.com>
2016-04-09 23:52:45 -0700

    PumpOps error handling cleanup

--- RileyLinkKit/PumpOps.swift
+++ RileyLinkKit/PumpOps.swift
@@ -24,14 +24,17 @@ public class PumpOps: NSObject {
   public func pressButton() {
     device.runSession { (session) -> Void in
       let ops = PumpOpsSynchronous(pumpState: self.pumpState, session: session)
-      ops.pressButton(.Down)
+      do {
+        try ops.pressButton(.Down)
+      } catch { }
     }
   }
   
   public func getPumpModel(completion: (String?) -> Void)  {
     device.runSession { (session) -> Void in
       let ops = PumpOpsSynchronous(pumpState: self.pumpState, session: session)
-      let model = ops.getPumpModel()
+      let model = try? ops.getPumpModel()
+
       dispatch_async(dispatch_get_main_queue(), { () -> Void in
         completion(model)
       })
@@ -41,7 +44,7 @@ public class PumpOps: NSObject {
   public func getBatteryVoltage(completion: (GetBatteryCarelinkMessageBody?) -> Void)  {
     device.runSession { (session) -> Void in
       let ops = PumpOpsSynchronous(pumpState: self.pumpState, session: session)
-      let response = ops.getBatteryVoltage()
+      let response = try? ops.getBatteryVoltage()
       dispatch_async(dispatch_get_main_queue(), { () -> Void in
         completion(response)
       })

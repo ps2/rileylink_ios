df2477fd9d65fea4e239efe2a59e62c47c617741
Nathan Racklyeft <loudnate@gmail.com>
2016-04-24 13:59:20 -0700

    Adding change notifications to PumpState

--- RileyLinkKit/PumpState.swift
+++ RileyLinkKit/PumpState.swift
@@ -11,11 +11,43 @@ import MinimedKit
 
 
 public class PumpState: NSObject {
+
+  /// Posted when values of the properties of the PumpState object have changed.
+  /// The `userInfo` dictionary contains the following keys: `PropertyKey` and `ValueChangeOldKey`
+  static let ValuesDidChangeNotification = "com.rileylink.RileyLinkKit.PumpState.ValuesDidChangeNotification"
+
+  /// The key for a string value naming the object property whose value changed
+  static let PropertyKey = "com.rileylink.RileyLinkKit.PumpState.PropertyKey"
+
+  /// The key for the previous value of the object property whose value changed.
+  /// If the value type does not conform to AnyObject, a raw representation will be provided instead.
+  static let ValueChangeOldKey = "com.rileylink.RileyLinkKit.PumpState.ValueChangeOldKey"
+
   public let pumpID: String
-  public var timeZone: NSTimeZone = NSTimeZone.defaultTimeZone()
-  public var pumpModel: PumpModel?
-  public var lastHistoryDump: NSDate?
-  public var awakeUntil: NSDate?
+
+  public var timeZone: NSTimeZone = NSTimeZone.defaultTimeZone() {
+    didSet {
+      postChangeNotificationForKey("timeZone", oldValue: oldValue)
+    }
+  }
+
+  public var pumpModel: PumpModel? {
+    didSet {
+      postChangeNotificationForKey("pumpModel", oldValue: oldValue?.name)
+    }
+  }
+
+  public var lastHistoryDump: NSDate? {
+    didSet {
+      postChangeNotificationForKey("lastHistoryDump", oldValue: oldValue)
+    }
+  }
+
+  public var awakeUntil: NSDate? {
+    didSet {
+      postChangeNotificationForKey("awakeUntil", oldValue: awakeUntil)
+    }
+  }
 
   public init(pumpID: String) {
     self.pumpID = pumpID
@@ -25,5 +57,20 @@ public class PumpState: NSObject {
 
   public var isAwake: Bool {
     return awakeUntil?.timeIntervalSinceNow > 0
+  }
+
+  private func postChangeNotificationForKey(key: String, oldValue: AnyObject?)  {
+    var userInfo: [String: AnyObject] = [
+      self.dynamicType.PropertyKey: key
+    ]
+
+    if let oldValue = oldValue {
+      userInfo[self.dynamicType.ValueChangeOldKey] = oldValue
+    }
+
+    NSNotificationCenter.defaultCenter().postNotificationName(self.dynamicType.ValuesDidChangeNotification,
+      object: self,
+      userInfo: userInfo
+    )
   }
 }

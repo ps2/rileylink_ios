080ce72bb2511ca07f36d88b4224f42439751188
Pete Schwamb <pete@schwamb.net>
2016-07-16 17:25:38 -0500

    Allow use of placeholder enum types for 7 byte pump events, while still keeping a useful event type name in the dictionary rep

--- MinimedKit/PumpEvents/PlaceholderPumpEvent.swift
+++ MinimedKit/PumpEvents/PlaceholderPumpEvent.swift
@@ -1,5 +1,5 @@
 //
-//  UnknownPumpEvent.swift
+//  PlaceholderPumpEvent.swift
 //  RileyLink
 //
 //  Created by Nate Racklyeft on 6/20/16.
@@ -9,10 +9,11 @@
 import Foundation
 
 
-public struct UnknownPumpEvent: TimestampedPumpEvent {
+public struct PlaceholderPumpEvent: TimestampedPumpEvent {
     public let length: Int
     public let rawData: NSData
     public let timestamp: NSDateComponents
+    public let name: String
 
     public init?(availableData: NSData, pumpModel: PumpModel) {
         length = 7
@@ -20,15 +21,21 @@ public struct UnknownPumpEvent: TimestampedPumpEvent {
         guard length <= availableData.length else {
             return nil
         }
-
+        
         rawData = availableData[0..<length]
 
         timestamp = NSDateComponents(pumpEventData: availableData, offset: 2)
+        
+        if let type = PumpEventType(rawValue: rawData[0] as UInt8) {
+            name = String(type).componentsSeparatedByString(".").last!
+        } else {
+            name = "UnknownPumpEvent(\(rawData[0] as UInt8))"
+        }
     }
 
     public var dictionaryRepresentation: [String: AnyObject] {
         return [
-            "_type": "UnknownPumpEvent(\(rawData[0] as UInt8))",
+            "_type": "\(name)",
         ]
     }
 }

1fa2a6dea4d3fbbc67ef57c1e3d165b3cddb4510
Nathan Racklyeft <loudnate@gmail.com>
2016-08-08 18:08:01 -0700

    Creating convenience initializers for Nightscout Status structs
    Using more natural arguments for the device/system, rather than surfacing NS design decisions any higher than necessary

--- NightscoutUploadKit/DeviceStatus/LoopSuggested.swift
+++ NightscoutUploadKit/DeviceStatus/LoopSuggested.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import HealthKit
 
 public struct LoopSuggested {
     let timestamp: NSDate
@@ -19,6 +20,22 @@ public struct LoopSuggested {
     let correction: Double?
     let predBGs: [Double]?
 
+    public init(timestamp: NSDate, rate: Double, duration: NSTimeInterval, eventualBG: HKQuantity, bg: HKQuantity, reason: String? = nil, tick: Int? = nil, correction: Double? = nil, predBGs: [HKQuantity]? = nil) {
+        // All nightscout data is in mg/dL.
+        let unit = HKUnit.milligramsPerDeciliterUnit()
+
+        self.init(
+            timestamp: timestamp,
+            rate: rate,
+            duration: duration,
+            eventualBG: Int(eventualBG.doubleValueForUnit(unit)),
+            bg: Int(bg.doubleValueForUnit(unit)),
+            reason: reason,
+            tick: tick,
+            correction: correction,
+            predBGs: predBGs?.map { $0.doubleValueForUnit(unit) }
+        )
+    }
 
     public init(timestamp: NSDate, rate: Double, duration: NSTimeInterval, eventualBG: Int, bg: Int, reason: String? = nil, tick: Int? = nil, correction: Double? = nil, predBGs: [Double]? = nil) {
         self.timestamp = timestamp

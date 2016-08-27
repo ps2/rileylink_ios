6767c514b41cf88fc8d3816a0a2a73d62875945b
Pete Schwamb <pete@schwamb.net>
2016-08-26 23:15:16 -0500

    Add SaveSettings pump event

--- MinimedKit/PumpEvents/SaveSettingsPumpEvent.swift
+++ MinimedKit/PumpEvents/SaveSettingsPumpEvent.swift
@@ -1,33 +1,33 @@
 //
-//  SelectBasalProfilePumpEvent.swift
+//  SaveSettingsPumpEvent.swift
 //  RileyLink
 //
-//  Created by Nate Racklyeft on 6/15/16.
+//  Created by Pete Schwamb on 8/26/16.
 //  Copyright © 2016 Pete Schwamb. All rights reserved.
 //
 
 import Foundation
 
-public struct SelectBasalProfilePumpEvent: TimestampedPumpEvent {
+public struct SaveSettingsPumpEvent: TimestampedPumpEvent {
     public let length: Int
     public let rawData: NSData
     public let timestamp: NSDateComponents
-
+    
     public init?(availableData: NSData, pumpModel: PumpModel) {
         length = 7
-
+        
         guard length <= availableData.length else {
             return nil
         }
-
+        
         rawData = availableData[0..<length]
-
+        
         timestamp = NSDateComponents(pumpEventData: availableData, offset: 2)
     }
-
+    
     public var dictionaryRepresentation: [String: AnyObject] {
         return [
-            "_type": "SelectBasalProfile",
+            "_type": "SaveSettings",
         ]
     }
 }

45c315c6103959f66c97508d5805d90c0ec0e4f1
Darrell Wright <dwright@mini2011.private>
2016-09-18 13:48:53 -0400

    Added Self Test Pump Event

--- MinimedKit/PumpEventType.swift
+++ MinimedKit/PumpEventType.swift
@@ -25,6 +25,7 @@ public enum PumpEventType: UInt8 {
     case setAutoOff = 0x1b
     case suspend = 0x1e
     case resume = 0x1f
+    case selftest = 0x20
     case rewind = 0x21
     case clearSettings = 0x22
     case changeChildBlockEnable = 0x23
@@ -112,6 +113,8 @@ public enum PumpEventType: UInt8 {
             return JournalEntryPumpLowBatteryPumpEvent.self
         case .battery:
             return BatteryPumpEvent.self
+        case .selftest:
+            return SelfTestPumpEvent.self
         case .suspend:
             return SuspendPumpEvent.self
         case .resume:

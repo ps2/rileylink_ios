550f13f274fc74a582998d08af77e318876e0aeb
Pete Schwamb <pete@schwamb.net>
2016-09-22 23:20:52 -0500

    Fix logic error

--- RileyLinkKit/PumpOpsSynchronous.swift
+++ RileyLinkKit/PumpOpsSynchronous.swift
@@ -481,7 +481,7 @@ class PumpOpsSynchronous {
                             NSLog("Found event (%@) out of order in history. Ending history fetch.", date as NSDate)
                             break pages
                         } else {
-                            if (date.compare(startDate) == .orderedAscending) {
+                            if (date.compare(startDate) != .orderedAscending) {
                                 timeCursor = date
                             }
                             events.insert(TimestampedHistoryEvent(pumpEvent: event, date: date), at: 0)

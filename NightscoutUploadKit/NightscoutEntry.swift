068522e409b48036684f22ba5e37b9a761ce3310
Tim Mecklem <tim@teamgaslight.com>
2016-11-20 12:48:13 -0500

    Only return nil if not a glucose event

--- NightscoutUploadKit/NightscoutEntry.swift
+++ NightscoutUploadKit/NightscoutEntry.swift
@@ -39,8 +39,9 @@ public class NightscoutEntry : DictionaryRepresentable {
     convenience init?(event: TimestampedGlucoseEvent, device: String) {
         if let glucoseSensorData = event.glucoseEvent as? GlucoseSensorDataGlucoseEvent {
             self.init(glucose: glucoseSensorData.sgv, timestamp: event.date, device: device, glucoseType: .Sensor)
+        } else {
+            return nil
         }
-        return nil
     }
     
     public var dictionaryRepresentation: [String: Any] {

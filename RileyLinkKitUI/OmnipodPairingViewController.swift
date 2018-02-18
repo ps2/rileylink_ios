976efa18950ead3f4897a06543b9c5dad106f8ed
Pete Schwamb <pete@schwamb.net>
2018-02-17 20:14:01 -0600

    Updating buttons

--- RileyLinkKitUI/OmnipodPairingViewController.swift
+++ RileyLinkKitUI/OmnipodPairingViewController.swift
@@ -10,9 +10,12 @@ import Foundation
 import OmniKit
 import RileyLinkBLEKit
 
+// Implementing flow as described here: https://app.moqups.com/pheltzel@gmail.com/GNBaAhrB1y/view/page/aa9df7b72
+
 public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
     
     private enum InteractionStates {
+        case initial
         case currentPodActive
         case noActivePod
         case fillNewPod
@@ -28,8 +31,10 @@ public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
         case pleaseWaitForActivePod
         case checkInfusionSite
         
-        var message: String {
+        var instructions: String {
             switch self {
+            case .noActivePod:
+                return NSLocalizedString("No active pod. Activate one now?", comment: "Message for no active pod.")
             case .fillNewPod:
                 return NSLocalizedString("Fill a new pod with insulin.\n\nAfter filling pod, listen for 2 beeps, then press \"Next.\"\n\nNOTE: Do not remove needle cap at this time.", comment: "Message for fill new pod screen")
             case .communicationError(let action, let error):
@@ -40,6 +45,28 @@ public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
                 return "Not implemented yet."
             }
         }
+        
+        var okButtonText: String? {
+            switch self {
+            case .noActivePod:
+                return NSLocalizedString("Yes", comment: "Affirmatitve response to question")
+            case .fillNewPod:
+                return NSLocalizedString("Next", comment: "Button text for next action")
+            default:
+                return nil
+            }
+        }
+
+        var cancelButtonText: String? {
+            switch self {
+            case .noActivePod:
+                return NSLocalizedString("No", comment: "Negative response to question")
+            case .fillNewPod:
+                return NSLocalizedString("Cancel", comment: "Button text to cancel")
+            default:
+                return nil
+            }
+        }
     }
     
     public let podComms: PodComms
@@ -47,22 +74,32 @@ public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
     
     private var interactionState: InteractionStates = .noActivePod {
         didSet {
-            
+            stepInstructions.text = interactionState.instructions
+            if let okText = interactionState.okButtonText {
+                okButton.setTitle(okText, for: .normal)
+                okButton.isHidden = false
+            } else {
+                okButton.isHidden = true
+            }
+            if let cancelText = interactionState.cancelButtonText {
+                cancelButton.setTitle(cancelText, for: .normal)
+                cancelButton.isHidden = false
+            } else {
+                cancelButton.isHidden = true
+            }
         }
     }
     
     @IBOutlet var progress: UIProgressView!
     @IBOutlet var stepInstructions: UITextView!
     @IBOutlet var titleLabel: UILabel!
-    
+    @IBOutlet var okButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+
     public init(podComms: PodComms, device: RileyLinkDevice) {
         self.podComms = podComms
         self.device = device
-        if podComms.podIsActive {
-            self.interactionState = .currentPodActive
-        } else {
-            self.interactionState = .noActivePod
-        }
+        self.interactionState = .initial
         
         super.init(nibName: OmnipodPairingViewController.className, bundle: Bundle(for: OmnipodPairingViewController.self))
     }
@@ -73,18 +110,30 @@ public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
 
     @IBAction func nextTapped(sender: UIButton) {
         switch interactionState {
+        case .noActivePod:
+            interactionState = .fillNewPod
         case .fillNewPod:
             initialPairing()
         default:
-            stepInstructions.text = "not implemented yet..."
+            stepInstructions.text = "\"\(String(describing: sender.title(for: .normal)))\" not handled for state \(String(describing: interactionState))"
         }
     }
     
     @IBAction func cancelTapped(sender: UIButton) {
+        switch interactionState {
+        default:
+            stepInstructions.text = "\"\(String(describing: sender.title(for: .normal)))\" not handled for state \(String(describing: interactionState))"
+        }
     }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
+        if podComms.podIsActive {
+            self.interactionState = .currentPodActive
+        } else {
+            self.interactionState = .noActivePod
+        }
     }
 
     func initialPairing() {
@@ -92,8 +141,13 @@ public class OmnipodPairingViewController: UIViewController, IdentifiableClass {
         podComms.runSession(withName: "Pairing new pod", using: device) { (session) in
             do {
                 try session.setupNewPOD()
+                DispatchQueue.main.async {
+                    self.interactionState = .priming
+                }
             } catch let error {
-                self.interactionState = .communicationError(during: "Address assignment", error: error)
+                DispatchQueue.main.async {
+                    self.interactionState = .communicationError(during: "Address assignment", error: error)
+                }
             }
         }
     }

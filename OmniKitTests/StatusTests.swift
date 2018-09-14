e973e5fb3407e79ad16e12330687fe37adfa5979
Eelke Jager <info@lytrix.com>
2018-09-14 22:10:15 +0200

    Added extra pod failure tests

--- OmniKitTests/StatusTests.swift
+++ OmniKitTests/StatusTests.swift
@@ -22,6 +22,9 @@ class StatusTests: XCTestCase {
     //    }
     //}
 
+    // end of suspend
+    // 0213010000000000000000000000000bd70c400000828c
+    
     func testStatusErrorNoFaultAlerts() {
     // 02 16 02 08 01 0000 0a 0038 00 0000 03ff 0087 00 00 00 95 ff 0000 //recorded an extra 81
     
@@ -51,7 +54,6 @@ class StatusTests: XCTestCase {
         
     }
     
-   
     func testStatusErrorFaultAlert() {
         // 02 16 02 0d 00 0000 06 0034 5c 0001 03ff 0001 00 00 05 a1 05 0186
         do {
@@ -77,8 +79,62 @@ class StatusTests: XCTestCase {
         } catch (let error) {
             XCTFail("message decoding threw error: \(error)")
         }
-        
     }
+    
+    func testStatusErrorDeliveryErrorDuringPriming() {
+        //0216 BODY:020f0000000900345c000103ff0001000005ae05602903
+        do {
+            // Decode
+            let decoded = try StatusError(encodedData: Data(hexadecimalString: "0216020f0000000900345c000103ff0001000005ae05602903")!)
+            XCTAssertEqual(.faultEvents, decoded.requestedType)
+            XCTAssertEqual(22, decoded.length)
+            XCTAssertEqual(.podInactive, decoded.progressType)
+            XCTAssertEqual(.none, decoded.deliveryInProgressType)
+            XCTAssertEqual(0000, decoded.insulinNotDelivered)
+            XCTAssertEqual(9, decoded.podMessageCounter)
+            XCTAssertEqual(92, decoded.originalLoggedFaultEvent)
+            XCTAssertEqual(0001*60, decoded.faultEventTimeSinceActivation)
+            XCTAssertEqual(51.15, decoded.insulinRemaining, accuracy: 0.05)
+            XCTAssertEqual(0001*60, decoded.timeActive, accuracy: 0) // timeActive converts minutes to seconds
+            XCTAssertEqual(00, decoded.secondaryLoggedFaultEvent)
+            XCTAssertEqual(false, decoded.logEventError)
+            XCTAssertEqual(.insulinStateCorruptionDuringErrorLogging, decoded.infoLoggedFaultEvent)
+            XCTAssertEqual(.readyForInjection, decoded.progressAtFirstLoggedFaultEvent)
+            XCTAssertEqual(10, decoded.recieverLowGain)
+            XCTAssertEqual(14, decoded.radioRSSI)
+            XCTAssertEqual(.readyForInjection, decoded.progressAtFirstLoggedFaultEventCheck)
+        } catch (let error) {
+            XCTFail("message decoding threw error: \(error)")
+        }
+    }
+    func testStatusErrorDuringPriming() {
+        // Needle cap accidentally removed before priming started leaking and gave error:
+        // 0216020d0000000600008f000003ff0000000003a20386a002
+        do {
+            // Decode
+            let decoded = try StatusError(encodedData: Data(hexadecimalString: "0216020d0000000600008f000003ff0000000003a20386a002")!)
+            XCTAssertEqual(.faultEvents, decoded.requestedType)
+            XCTAssertEqual(22, decoded.length)
+            XCTAssertEqual(.errorEventLoggedShuttingDown, decoded.progressType)
+            XCTAssertEqual(.none, decoded.deliveryInProgressType)
+            XCTAssertEqual(0000, decoded.insulinNotDelivered)
+            XCTAssertEqual(6, decoded.podMessageCounter)
+            XCTAssertEqual(143, decoded.originalLoggedFaultEvent)
+            XCTAssertEqual(0000*60, decoded.faultEventTimeSinceActivation)
+            XCTAssertEqual(51.15, decoded.insulinRemaining, accuracy: 0.05)
+            XCTAssertEqual(0000*60, decoded.timeActive, accuracy: 0) // timeActive converts minutes to seconds
+            XCTAssertEqual(00, decoded.secondaryLoggedFaultEvent)
+            XCTAssertEqual(false, decoded.logEventError)
+            XCTAssertEqual(.insulinStateCorruptionDuringErrorLogging, decoded.infoLoggedFaultEvent)
+            XCTAssertEqual(.pairingSuccess, decoded.progressAtFirstLoggedFaultEvent)
+            XCTAssertEqual(10, decoded.recieverLowGain)
+            XCTAssertEqual(2, decoded.radioRSSI)
+            XCTAssertEqual(.pairingSuccess, decoded.progressAtFirstLoggedFaultEventCheck)
+        } catch (let error) {
+            XCTFail("message decoding threw error: \(error)")
+        }
+    }
+    
     
     func testStatusRequestCommand() {
         // 0e 01 00

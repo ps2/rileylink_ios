1a2dc76bcc806dae465edcb8a1f7c01214f70950
Eelke Jager <info@lytrix.com>
2018-09-25 23:03:41 +0200

    Renamed faultEvent>faultEventType

--- OmniKit/MessageTransport/MessageBlocks/PodInfoDataLog.swift
+++ OmniKit/MessageTransport/MessageBlocks/PodInfoDataLog.swift
@@ -17,13 +17,15 @@ public struct PodInfoDataLog : PodInfo {
     
     public var blockType                : MessageBlockType = .podInfoResponse
     public var podInfoType              : PodInfoResponseSubType = .dataLog
-    public let length                   : UInt8 //=dataChunckSize * N + 8 where N is the number of dword entries
-    public let loggedFaultEvent         : PodInfoResponseSubType.FaultEventType
+    public let length                   : UInt8
+    public let faultEventType           : PodInfoResponseSubType.FaultEventType
     public let data                     : Data
     public let timeFaultEvent           : TimeInterval
     public let timeActivation           : TimeInterval
     public let dataChunkSize            : UInt8
     public let dataChunkWords           : UInt8
+    // TODO adding a datadump variable based on length
+    //length = dataChunckSize * N + 8 where N is the number of dword entries
     // public let loggedData               : Data
     public init(encodedData: Data) throws {
         
@@ -33,7 +35,7 @@ public struct PodInfoDataLog : PodInfo {
         self.blockType           = MessageBlockType(rawValue: encodedData[0])!
         self.length              = encodedData[1]
         self.podInfoType         = PodInfoResponseSubType(rawValue: encodedData[2])!
-        self.loggedFaultEvent    = PodInfoResponseSubType.FaultEventType(rawValue: encodedData[3])!
+        self.faultEventType      = PodInfoResponseSubType.FaultEventType(rawValue: encodedData[3])!
         self.timeFaultEvent      = TimeInterval(minutes: Double((Int(encodedData[4] & 0b1) << 8) + Int(encodedData[5])))
         self.timeActivation      = TimeInterval(minutes: Double((Int(encodedData[6] & 0b1) << 8) + Int(encodedData[7])))
         self.dataChunkSize       = encodedData[8]

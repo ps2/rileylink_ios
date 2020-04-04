f9b04b1a222bf2a2b2c70ce6b65464291b5b3c9a
Joe Moran <mojo@moranfoundation.org>
2020-04-04 11:21:44 -0700

    Clean up work to better match wiki (#594)
    * Cleanup work to better match wiki

--- OmniKit/MessageTransport/MessageBlocks/PodInfo.swift
+++ OmniKit/MessageTransport/MessageBlocks/PodInfo.swift
@@ -21,8 +21,6 @@ public enum PodInfoResponseSubType: UInt8, Equatable {
     case faultEvents                 = 0x02
     case dataLog                     = 0x03
     case fault                       = 0x05
-    case hardcodedTestValues         = 0x06
-    case resetStatus                 = 0x46 // including state, initialization time, any faults
     case pulseLogRecent              = 0x50 // dumps up to 50 entries data from the pulse log
     case dumpOlderPulseLog           = 0x51 // like 0x50, but dumps entries before the last 50
     
@@ -38,10 +36,6 @@ public enum PodInfoResponseSubType: UInt8, Equatable {
             return PodInfoDataLog.self
         case .fault:
             return PodInfoFault.self
-        case .hardcodedTestValues:
-            return PodInfoTester.self
-        case .resetStatus:
-            return PodInfoResetStatus.self
         case .pulseLogRecent:
             return PodInfoPulseLogRecent.self
         case .dumpOlderPulseLog:

a98a7fe224c831ed356e585f9fafd3f76e5aa761
Joe Moran <mojo@moranfoundation.org>
2020-10-03 01:40:25 -0700

    Improved display of Optional previousPodProgressStatus value

--- OmniKitUI/ViewControllers/CommandResponseViewController.swift
+++ OmniKitUI/ViewControllers/CommandResponseViewController.swift
@@ -81,7 +81,9 @@ extension CommandResponseViewController {
         if status.faultEventCode.faultType != .noFaults {
             result += "\n" // since we have a fault, report the additional fault related information in a separate section
             result += String(format: LocalizedString("Fault: %1$@\n", comment: "The format string for a fault: (1: The fault description)"), status.faultEventCode.localizedDescription)
-            result += String(format: LocalizedString("Previous pod progress: %1$@\n", comment: "The format string for previous pod progress: (1: previous pod progress string)"), String(describing: status.previousPodProgressStatus))
+            if let previousPodProgressStatus = status.previousPodProgressStatus {
+                result += String(format: LocalizedString("Previous pod progress: %1$@\n", comment: "The format string for previous pod progress: (1: previous pod progress string)"), String(describing: previousPodProgressStatus))
+            }
             if let faultEventTimeSinceActivation = status.faultEventTimeSinceActivation, let faultTimeStr = formatter.string(from: faultEventTimeSinceActivation) {
                 result += String(format: LocalizedString("Fault time: %1$@\n", comment: "The format string for fault time: (1: fault time string)"), faultTimeStr)
             }

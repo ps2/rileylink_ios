68feed92c20b3cb7dedab93d871320b29918863d
elnjensen <ejensen1@swarthmore.edu>
2021-01-01 17:35:00 -0500

    Add top-level mySentry variable to pump manager

--- MinimedKit/PumpManager/MinimedPumpManager.swift
+++ MinimedKit/PumpManager/MinimedPumpManager.swift
@@ -694,7 +694,19 @@ extension MinimedPumpManager {
             }
         }
     }
-    
+
+    /// Whether to use MySentry packets on capable pumps:
+    public var useMySentry: Bool {
+        get {
+            return state.useMySentry
+        }
+        set {
+            setState { (state) in
+                state.useMySentry = newValue
+            }
+        }
+    }
+
 }
 
 

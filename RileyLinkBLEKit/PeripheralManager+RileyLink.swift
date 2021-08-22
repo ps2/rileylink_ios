46ed226bbb683b5e968388756d22a4be222d712e
Pete Schwamb <pete@schwamb.net>
2021-08-21 20:01:13 -0500

    Log response when unable to parse

--- RileyLinkBLEKit/PeripheralManager+RileyLink.swift
+++ RileyLinkBLEKit/PeripheralManager+RileyLink.swift
@@ -565,7 +565,7 @@ extension PeripheralManager {
                         return false
                     default:
                         guard let response = R(data: value) else {
-                            log.error("Unable to parse response.")
+                            log.error("Unable to parse response: %@", value.hexadecimalString)
                             // We don't recognize the contents. Keep listening.
                             return false
                         }

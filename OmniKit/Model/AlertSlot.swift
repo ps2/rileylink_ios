caad1e524ffc2d6629813fc387e181a6c6d60e46
Joe Moran <mojo@moranfoundation.org>
2022-02-07 18:13:25 -0800

    Fixes for suspend reminder handling when [auto] resuming pod (#699)
    + Properly handle status updates after cancelling suspend reminders
    + Have setBasalSchedule() call cancelSuspendAlerts() as needed
    + Active suspend alerts will now be cancelled for all auto resume cases
    when suspended (manual bolus, Change Time Zone, Basal Rates edit)
    + Don't send alert commands when resuming with no active suspend alerts

--- OmniKit/Model/AlertSlot.swift
+++ OmniKit/Model/AlertSlot.swift
@@ -364,3 +364,12 @@ public struct AlertSet: RawRepresentable, Collection, CustomStringConvertible, E
         }
     }
 }
+
+// Returns true if there are any active suspend related alerts
+public func hasActiveSuspendAlert(configuredAlerts: [AlertSlot : PodAlert]) -> Bool {
+    // slot5 is for podSuspendedReminder and slot6 is for suspendTimeExpired
+    if configuredAlerts.contains(where: { ($0.key == .slot5 || $0.key == .slot6) && $0.value.configuration.active }) {
+        return true
+    }
+    return false
+}

b33df124610943b4ae21c38e226c4c02ec9a9115
Marion Barker <marionbarker@earthlink.net>
2022-12-04 08:34:23 -0800

    For HUD lifecycle display trigger, use scheduledExpirationReminderOffset (#752)
    * For HUD lifecycle display trigger, use scheduledExpirationReminderOffset
    
    * Update Pod lifecycle color, use selected reminder time

--- OmniKitUI/ViewModels/OmnipodSettingsViewModel.swift
+++ OmniKitUI/ViewModels/OmnipodSettingsViewModel.swift
@@ -513,8 +513,9 @@ extension OmnipodPumpManager {
                 return .expired
             default:
                 let remaining = Pod.nominalPodLife - (status.faultEventTimeSinceActivation ?? Pod.nominalPodLife)
+                let podTimeUntilReminder = remaining - (state.scheduledExpirationReminderOffset ?? 0)
                 if remaining > 0 {
-                    return .timeRemaining(remaining)
+                    return .timeRemaining(timeUntilExpiration: remaining, timeUntilExpirationReminder: podTimeUntilReminder)
                 } else {
                     return .expired
                 }
@@ -529,7 +530,8 @@ extension OmnipodPumpManager {
         case .active:
             if let podTimeRemaining = podTimeRemaining {
                 if podTimeRemaining > 0 {
-                    return .timeRemaining(podTimeRemaining)
+                    let podTimeUntilReminder = podTimeRemaining - (state.scheduledExpirationReminderOffset ?? 0)
+                    return .timeRemaining(timeUntilExpiration: podTimeRemaining, timeUntilExpirationReminder: podTimeUntilReminder)
                 } else {
                     return .expired
                 }
